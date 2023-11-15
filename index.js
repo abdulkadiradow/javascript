a68a711604de9e5e380ee735fe82ff89bcac0a3b
Abdulkadir Adow <abdul.adow1@gmail.com>
2023-11-15 21:06:31 +0000

    "commit"

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+
+// object orientated programming
 var purchase1 = {
   shoes: 100,
   stateTax: 1.2,
-  totalPrice: function () {
+  totalPrice: function() {
     var calculation = this.shoes * this.stateTax;
     console.log("total price:", calculation);
   },
 }
+purchase1.totalPrice();
 
 var purchase1 = {
   shoes: 100,
@@ -15,3 +18,15 @@ var purchase1 = {
     console.log('total price:', calculation);
   }
 }
+purchase1.totalPrice();
+// Functional programming
+
+var shoes = 100;
+var stateTax = 1.2;
+
+function totalPrice(shoes, stateTax){
+  return shoes * stateTax;
+
+}
+var toPay = totalPrice(shoes, stateTax);
+console.log(toPay);
